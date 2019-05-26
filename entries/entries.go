1a2b78fa69c24a02f4cc659307d564f395a46785
Joe Gregorio <joe@bitworking.org>
2019-05-25 21:48:58 -0400

    compiles

---
+++ entries/entries.go
@@ -0,0 +1,98 @@
+package entries
+
+import (
+	"context"
+	"crypto/md5"
+	"fmt"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"time"
+
+	"google.golang.org/api/iterator"
+
+	"github.com/jcgregorio/go-lib/ds"
+	"github.com/jcgregorio/slog"
+)
+
+const (
+	ENTRY ds.Kind = "Entry"
+)
+
+type Entries struct {
+	DS  *ds.DS
+	log slog.Logger
+}
+
+func New(ctx context.Context, project, ns string, log slog.Logger) (*Entries, error) {
+	d, err := ds.New(ctx, project, ns)
+	if err != nil {
+		return nil, err
+	}
+	return &Entries{
+		DS:  d,
+		log: log,
+	}, nil
+}
+
+type Entry struct {
+	Title   string    `datastore:",noindex"`
+	Content string    `datastore:",noindex"`
+	ID      string    `datastore:"-"`
+	Created time.Time `datastore:",Created"`
+}
+
+func (e *Entries) Get(ctx context.Context, id string) (*Entry, error) {
+	key := e.DS.NewKey(ENTRY)
+	key.Name = id
+
+	var entry Entry
+	if err := e.DS.Client.Get(ctx, key, &entry); err != nil {
+		return nil, fmt.Errorf("Failed to load %s: %s", key, err)
+	} else {
+		entry.ID = id
+		return &entry, nil
+	}
+}
+
+func (e *Entries) Insert(ctx context.Context, content, title string) (string, error) {
+	key := e.DS.NewKey(ENTRY)
+	key.Name = fmt.Sprintf("%x", md5.Sum([]byte(content+title)))
+
+	entry := &Entry{
+		Content: content,
+		Title:   title,
+		Created: time.Now(),
+	}
+	_, err := e.DS.Client.Put(context.Background(), key, entry)
+	return key.Name, err
+}
+
+func (e *Entries) Update(ctx context.Context, id, content, title string) error {
+	return nil
+}
+
+func (e *Entries) Delete(ctx context.Context, id string) error {
+	return nil
+}
+
+func (e *Entries) List(ctx context.Context, n int) ([]*Entry, error) {
+	ret := []*Entry{}
+	q := e.DS.NewQuery(ENTRY).Order("-Created").Limit(n)
+
+	it := e.DS.Client.Run(ctx, q)
+	for {
+		entry := &Entry{}
+		key, err := it.Next(entry)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			e.log.Infof("Failed while reading: %s", err)
+			break
+		}
+		entry.ID = key.Name
+		ret = append(ret, entry)
+	}
+	return ret, nil
+}
---
+++ entries/entries_test.go
@@ -0,0 +1,52 @@
+package entries
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/jcgregorio/logger"
+	"github.com/stretchr/testify/assert"
+)
+
+// InitDatastore is a common utility function used in tests. It sets up the
+// datastore to connect to the emulator and also clears out all instances of
+// the given 'kinds' from the datastore.
+func InitForTesting(t assert.TestingT) *Mentions {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	emulatorHost := os.Getenv("DATASTORE_EMULATOR_HOST")
+	if emulatorHost == "" {
+		assert.Fail(t, `Running tests that require a running Cloud Datastore emulator.
+
+Run
+
+	"gcloud beta emulators datastore start --no-store-on-disk --host-port=localhost:8888"
+
+and then run
+
+  $(gcloud beta emulators datastore env-init)
+
+to set the environment variables. When done running tests you can unset the env variables:
+
+  $(gcloud beta emulators datastore env-unset)
+
+`)
+	}
+
+	// Do a quick healthcheck against the host, which will fail immediately if it's down.
+	_, err := http.DefaultClient.Get("http://" + emulatorHost + "/")
+	assert.NoError(t, err, fmt.Sprintf("Cloud emulator host %s appears to be down or not accessible.", emulatorHost))
+
+	m, err := New(context.Background(), "test-project", fmt.Sprintf("test-namespace-%d", r.Uint64()), logger.New())
+	assert.NoError(t, err)
+	return m
+}
+
+func TestDB(t *testing.T) {
+	m := InitForTesting(t)
+
+}
